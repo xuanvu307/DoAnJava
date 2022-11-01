9caa4ccb3fdae115a40482e64b9a9457caa75c5b
xuanvu307 <xuanvufb2@gmail.com>
2022-11-01 21:21:33 +0700

    Update Main.java

--- src/control/Main.java
+++ src/control/Main.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Scanner
-
-    
-
-
-        
-=======
->>>>>>> 8ea479d6078a579adc342271d261906d51725a50
+        public void 
     }
 }
