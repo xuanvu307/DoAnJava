c2b941f3fdc9dac3dda5581643f8faba1f01b8ec
BachBox <101316321+BachBox@users.noreply.github.com>
2022-11-03 21:18:45 +0700

    sua main
    213

--- src/control/Main.java
+++ src/control/Main.java
@@ -2,7 +2,8 @@ package control;
 
 public class Main {
     public static void main(String[] args) {
-
+        int 1234;
+        
         long l = System.currentTimeMillis();
         System.out.println(l);
     }
