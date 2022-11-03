0dc613da0a56443de65f6c822bff3cfe0b821601
xuanvu307 <xuanvufb2@gmail.com>
2022-11-03 21:20:52 +0700

    Update Main.java

--- src/control/Main.java
+++ src/control/Main.java
@@ -2,9 +2,9 @@ package control;
 
 public class Main {
     public static void main(String[] args) {
-        int 1234;
-        
+
         long l = System.currentTimeMillis();
         System.out.println(l);
+        System.out.println(l);System.out.println(l);
     }
 }
